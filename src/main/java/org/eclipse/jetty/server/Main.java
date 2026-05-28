394935af984e4385575e7b3353de374d288647ed
LKH <asdf913@gail.com>
2026-05-29 00:08:39 +0900

    Add "length(java.lang.Object[])","put(java.util.Map,java.lang.Object,java.lang.Object)" and "toMap(java.lang.String...)" methods in "org.eclipse.jetty.server.Main" class Update "main(java.lang.String[])" method in "org.eclipse.jetty.server.Main" class

--- src/main/java/org/eclipse/jetty/server/Main.java
+++ src/main/java/org/eclipse/jetty/server/Main.java
@@ -1,7 +1,16 @@
 package org.eclipse.jetty.server;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
 import javax.servlet.http.MainServlet;
 
+import org.apache.commons.collections4.MapUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.ObjectUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
@@ -15,7 +24,7 @@ public class Main {
 		//
 		servletContextHandler.addServlet(new ServletHolder(new MainServlet()), "/");
 		//
-		final Server server = new Server(8080);
+		final Server server = new Server(NumberUtils.toInt(MapUtils.getString(toMap(args), "port"), 8080));
 		//
 		server.setHandler(servletContextHandler);
 		//
@@ -35,6 +44,54 @@ public class Main {
 		} catch (final ClassNotFoundException e) {
 			return false;
 		}
+	}
+
+	private static Map<String, String> toMap(final String... ss) {
+		//
+		String s = null;
+		//
+		Map<String, String> map = null;
+		//
+		for (int i = 0; i < length(ss); i++) {
+			//
+			if (Objects.equals(s = ArrayUtils.get(ss, i), "=")) {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), "", "");
+				//
+			} else if (s != null && s.length() == 2 && s.charAt(0) == '=') {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), "", s.substring(1, s.length()));
+				//
+			} else if (s != null && s.length() == 2 && s.charAt(s.length() - 1) == '=') {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), s.substring(0, s.length() - 1), "");
+				//
+			} else if (s != null && s.indexOf('=') >= 0 && s.indexOf('=') == s.lastIndexOf('=')) {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), StringUtils.substringBefore(s, '='),
+						StringUtils.substringAfter(s, '='));
+				//
+			} else if (s != null && s.length() > 2 && s.indexOf('=') != s.lastIndexOf('=')) {
+				//
+				put(map = ObjectUtils.getIfNull(map, LinkedHashMap::new), StringUtils.substring(s, 0, s.indexOf('=')),
+						StringUtils.substring(s, s.indexOf('=') + 1));
+				//
+			} // if
+				//
+		} // for
+			//
+		return map;
+		//
+	}
+
+	private static <K, V> void put(final Map<K, V> instance, final K key, final V value) {
+		if (instance != null) {
+			instance.put(key, value);
+		}
+	}
+
+	private static int length(final Object[] instance) {
+		return instance != null ? instance.length : 0;
 	}
 
 }
--- src/test/java/org/eclipse/jetty/server/MainTest.java
+++ src/test/java/org/eclipse/jetty/server/MainTest.java
@@ -1,17 +1,29 @@
 package org.eclipse.jetty.server;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Objects;
 
 import org.apache.commons.lang3.ArrayUtils;
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import io.github.toolfactory.narcissus.Narcissus;
 
 class MainTest {
+
+	private static Method METHOD_TO_MAP = null;
+
+	@BeforeSuite
+	void beforeSuite() throws NoSuchMethodException {
+		//
+		(METHOD_TO_MAP = Main.class.getDeclaredMethod("toMap", String[].class)).setAccessible(true);
+		//
+	}
 
 	@Test
 	void testNull() throws Throwable {
@@ -36,7 +48,7 @@ class MainTest {
 			//
 			result = Narcissus.invokeStaticMethod(m, toArray(Collections.nCopies(m.getParameterCount(), null)));
 			//
-			if (Objects.equals(m.getReturnType(), Boolean.TYPE)) {
+			if (contains(Arrays.asList(Boolean.TYPE, Integer.TYPE), m.getReturnType())) {
 				//
 				Assert.assertNotNull(result, toString);
 				//
@@ -50,8 +62,37 @@ class MainTest {
 			//
 	}
 
+	private static boolean contains(final Collection<?> items, final Object item) {
+		return items != null && items.contains(item);
+	}
+
 	private static Object[] toArray(final Collection<?> instance) {
 		return instance != null ? instance.toArray() : null;
+	}
+
+	@Test
+	public void testToMap() throws IllegalAccessException, InvocationTargetException {
+		//
+		Assert.assertEquals(invoke(METHOD_TO_MAP, null, (Object) new String[] { "=" }),
+				Collections.singletonMap("", ""));
+		//
+		Assert.assertEquals(invoke(METHOD_TO_MAP, null, (Object) new String[] { " =" }),
+				Collections.singletonMap(" ", ""));
+		//
+		Assert.assertEquals(invoke(METHOD_TO_MAP, null, (Object) new String[] { "= " }),
+				Collections.singletonMap("", " "));
+		//
+		Assert.assertEquals(invoke(METHOD_TO_MAP, null, (Object) new String[] { " = " }),
+				Collections.singletonMap(" ", " "));
+		//
+		Assert.assertEquals(invoke(METHOD_TO_MAP, null, (Object) new String[] { "== " }),
+				Collections.singletonMap("", "= "));
+		//
+	}
+
+	private static Object invoke(final Method method, final Object instance, final Object... args)
+			throws IllegalAccessException, InvocationTargetException {
+		return method != null ? method.invoke(instance, args) : null;
 	}
 
 }
